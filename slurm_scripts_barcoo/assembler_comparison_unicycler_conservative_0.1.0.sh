be0ef9a43bdac3f38c4290a7206dead2e111b040
Ryan Wick <rrwick@gmail.com>
2017-01-06 00:00:09 +1100

    Fix Unicycler SLURM scripts for Barcoo

--- slurm_scripts_barcoo/assembler_comparison_unicycler_conservative_0.1.0.sh
+++ slurm_scripts_barcoo/assembler_comparison_unicycler_conservative_0.1.0.sh
@@ -15,7 +15,7 @@ module load python-gcc/3.5.1
 
 export PATH="/vlsci/SG0006/rwick/SPAdes-3.9.1-Linux/bin:$PATH"
 export PATH="/vlsci/SG0006/rwick/graphmap/bin/Linux-x64:$PATH"
-export PATH="/vlsci/SG0006/rwick/unicycler_v0.1.0:$PATH"
+export PATH="/vlsci/SG0006/rwick/unicycler_v0.1.0-barcoo:$PATH"
 export PATH="/vlsci/SG0006/rwick/quast-4.4-barcoo:$PATH"
 export PATH="/scratch/sysgen/rwick/Unicycler-assembly-tests:$PATH"
 
